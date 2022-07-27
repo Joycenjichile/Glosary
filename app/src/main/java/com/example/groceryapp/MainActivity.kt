06dc4471d4d3bcfbf55d14d62f3f09007c97b224
Joycenjichile <joycenkimalantoi@gmail.com>
2022-07-27 11:58:53 +0300

    changes

--- app/src/main/java/com/example/groceryapp/MainActivity.kt
+++ app/src/main/java/com/example/groceryapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.groceryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.groceryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnNext.setOnClickListener {
+            val intent= Intent(this,OrderActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/groceryapp/OrderActivity2.kt
+++ app/src/main/java/com/example/groceryapp/OrderActivity2.kt
@@ -1,11 +1,20 @@
 package com.example.groceryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.groceryapp.databinding.ActivityMainBinding
+import com.example.groceryapp.databinding.ActivityOrder2Binding
 
 class OrderActivity2 : AppCompatActivity() {
+    lateinit var binding:ActivityOrder2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order2)
+        binding= ActivityOrder2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnNt.setOnClickListener {
+            val intent= Intent(this,PriceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
